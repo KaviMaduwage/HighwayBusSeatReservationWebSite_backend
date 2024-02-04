32c178450d8c46873187332bbc1346164307ccc4
KaviMaduwage <kavindimaduwage@gmail.com>
2024-02-04 12:17:24 -0800

    initial commit

---
+++ src/test/java/com/project/seatReservation/SeatReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.seatReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
